1c7f2f53febbfc9c4ff972493c9c08dc0d967b80
zuppdev <69706835+zuppdev@users.noreply.github.com>
2025-11-25 14:17:06 +0800

    Initial Commit

---
+++ ArtGalleryAppBINB/ArtGalleryAppBINBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGalleryAppBINBApp.swift
+//  ArtGalleryAppBINB
+//
+//  Created by Zap on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ArtGalleryAppBINBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
